c86457bb43566716dcfee99e45ccf4ce4d4624fd
VitorLSBrandao <vitor.brandao@eduzz.com>
2021-08-29 13:01:41 -0300

    add login with github

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { SignInButton } from '../SignInButton';
+
+export function Header() {
+    return (
+        <header>
+            <div >
+                <SignInButton />
+            </div>
+        </header>
+    );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { Provider as NextAuthProvider } from 'next-auth/client'
 import { AppProps } from 'next/app'
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
+      <Header />
       <Component {...pageProps} />
     </NextAuthProvider >
   );
