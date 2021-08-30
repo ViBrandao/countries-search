5b5b56e4bc63868831423953afe925f0c3840487
VitorLSBrandao <vitor.brandao@eduzz.com>
2021-08-29 22:49:15 -0300

    add draft of search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,40 @@
+import { FormEvent, useState } from "react";
+import { api } from "../services/api";
+
+interface ICountry {
+  name: string;
+  capital: string;
+}
+
 export default function Home() {
+  const [countryName, setCountryName] = useState('');
+  const [countries, setCountries] = useState<ICountry[]>([]);
+  console.log(countries);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    if (countryName) {
+      api
+        .get(`name/${countryName}`)
+        .then((response) => {
+          setCountries(response.data);
+        });
+    }
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Search Countries</h1>
+        <input type="text" onChange={(event) => setCountryName(event.target.value)} />
+      </form>
+      {countries && (countries.map((country, index) => (
+        <div key={index}>
+          <span>Name: {country.name}</span>
+          <br />
+          <span>Capital: {country.capital}</span>
+        </div>
+      )))}
+    </div>
   )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: '/api'
+    baseURL: 'https://restcountries.eu/rest/v2/'
 });
